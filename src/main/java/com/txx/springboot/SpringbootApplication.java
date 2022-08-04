c60017591d38c96161952451ae66f03684a0080c
tangxiang <tangx1105@163.com>
2022-08-04 19:16:15 +0800

    Springboot框架搭建

---
+++ src/main/java/com/txx/springboot/SpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.txx.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+    @GetMapping("/")
+    public String index(){
+        return "ok";
+    }
+}
+
