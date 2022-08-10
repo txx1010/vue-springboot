947c462f6f1c4401e3d2eca90ca5a3f4c2c66226
tangxiang <tangx1105@163.com>
2022-08-10 19:45:14 +0800

    springboot集成Mybaits-Plus和SwaggerUI(采用Mybaits-Plus来代替上一节的mybaits实现增删改查分页)

--- src/main/java/com/txx/springboot/entity/User.java
+++ src/main/java/com/txx/springboot/entity/User.java
@@ -1,17 +1,21 @@
 package com.txx.springboot.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-//可以不添加get和set方法（lombok插件）
-@NoArgsConstructor
-//可以帮助创建无参构造方法
-@AllArgsConstructor
-//可以帮助创建有参构造方法
+@Data//可以不添加get和set方法（lombok插件）
+@NoArgsConstructor//可以帮助创建无参构造方法
+@AllArgsConstructor//可以帮助创建有参构造方法
+@TableName(value = "sys_user")
 public class User {
+
+    @TableId(type= IdType.AUTO)
     private Integer id;
     private String username;
     @JsonIgnore
@@ -21,6 +25,7 @@ public class User {
     private String phone;
     private String address;
 
-
+    @TableField(value = "avatar_url")//通过指定别名，可以让数据库中的名字和实体类的名字不一致
+    private String avatarUrl;
 
 }
