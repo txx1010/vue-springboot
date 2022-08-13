009d2f60964463f104ab6482fe95bded20ed5596
tangxiang <tangx1105@163.com>
2022-08-13 23:00:41 +0800

    Vue使用路由

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from '@/store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/global.css'
@@ -14,5 +15,6 @@ Vue.prototype.request=request
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,22 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import store from '@/store'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    component: () => import('../views/Manage.vue'),
+    redirect:"/home",
+    children:[
+      {path: 'home', name: '首页', component: () => import('../views/Home.vue')},
+      {path: 'user', name: '用户管理', component: () => import('../views/User.vue')},
+    ]
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
@@ -24,6 +31,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+//路由守卫
+router.beforeEach((to, from, next) =>{
+  localStorage.setItem("currentPathName",to.name) //设置当前的路由,为了在Header组件中使用
+  store.commit("setPath")     //触发store的数据更新
+  next()  //放行路由
 })
 
 export default router
