3afb5446d4c8fda7805e410b45ccdaf0010582ca
tangxiang <tangx1105@163.com>
2022-08-26 18:12:35 +0800

    权限菜单（动态路由bug修复）

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -18,7 +18,7 @@ const routes = [
     component: () => import('../views/Register.vue')
   },
   {
-    path: '*',
+    path: '/404',
     name: '404',
     component: () => import('../views/404.vue')
   },
@@ -30,29 +30,41 @@ const router = new VueRouter({
   routes
 })
 
+// 提供一个重置路由的方法
+export const resetRouter = () => {
+  router.matcher = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+  })
+}
+
 export const setRouters = () =>{
   const storeMenus=localStorage.getItem("menus");
   if(storeMenus) {
-    //拼装动态路由
-    const manageRoute = {path: '/', name:'Manage',component: () => import('../views/Manage.vue'), redirect:"/home", children:[]}
-    const menus = JSON.parse(storeMenus)
-    menus.forEach(item =>{
-      if(item.path){    //当且仅当path不为空的时候才去设置路由
-        let itemMenu = {path:item.path.replace("/",""),name: item.name,component:() => import('../views/' + item.pagePath + '.vue')}
-        manageRoute.children.push(itemMenu)
-      } else if(item.children.length) {
-        item.children.forEach(item =>{
-          if(item.path){
-            let itemMenu = {path:item.path.replace("/",""),name: item.name,component:() => import('../views/' + item.pagePath + '.vue')}
-            manageRoute.children.push(itemMenu)
-          }
-        })
-        }
-    })
 
     //获取当前的路由名称数组
     const currentRouteNames = router.getRoutes().map(v => v.name)
     if(!currentRouteNames.includes('Manage')){
+      //拼装动态路由
+      const manageRoute = {path: '/', name:'Manage',component: () => import('../views/Manage.vue'), redirect:"/home", children:[
+          { path: 'person', name: '个人信息', component: () => import('../views/Person.vue')},
+          { path: 'password', name: '修改密码', component: () => import('../views/Password.vue')}
+        ]}
+      const menus = JSON.parse(storeMenus)
+      menus.forEach(item =>{
+        if(item.path){    //当且仅当path不为空的时候才去设置路由
+          let itemMenu = {path:item.path.replace("/",""),name: item.name,component:() => import('../views/' + item.pagePath + '.vue')}
+          manageRoute.children.push(itemMenu)
+        } else if(item.children.length) {
+          item.children.forEach(item =>{
+            if(item.path){
+              let itemMenu = {path:item.path.replace("/",""),name: item.name,component:() => import('../views/' + item.pagePath + '.vue')}
+              manageRoute.children.push(itemMenu)
+            }
+          })
+        }
+      })
       //动态添加到现在的路由对象中去
       router.addRoute(manageRoute)
     }
@@ -66,6 +78,17 @@ setRouters()
 router.beforeEach((to, from, next) =>{
   localStorage.setItem("currentPathName",to.name) //设置当前的路由,为了在Header组件中使用
   store.commit("setPath")     //触发store的数据更新
+
+  //未找到路由的情况
+  if(!to.matched.length){
+    const storeMenus = localStorage.getItem("menus");
+    if(storeMenus){
+      next("/404")
+    } else {
+      next("/login")
+    }
+  }
+  //其他的情况都放行
   next()  //放行路由
 })
 
--- vue/src/views/Menu.vue
+++ vue/src/views/Menu.vue
@@ -1,16 +1,15 @@
- <template>
+<template>
   <div>
-
-    <div style="padding: 10px 0">
+    <div style="margin: 10px 0">
       <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search" v-model="name"></el-input>
-<!--      <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message" class="ml-5" v-model="email"></el-input>-->
-<!--      <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5" v-model="address"></el-input>-->
+      <!--      <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message" class="ml-5" v-model="email"></el-input>-->
+      <!--      <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5" v-model="address"></el-input>-->
       <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
-      <el-button  type="warning" @click="reset">重置</el-button>
+      <el-button type="warning" @click="reset">重置</el-button>
     </div>
 
     <div style="margin: 10px 0">
-      <el-button type="primary" @click="handleAdd()">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+      <el-button type="primary" @click="handleAdd(null)">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
       <el-popconfirm
           class="ml-5"
           confirm-button-text='确定'
@@ -22,27 +21,27 @@
       >
         <el-button type="danger" slot="reference">批量删除 <i class="el-icon-remove-outline"></i></el-button>
       </el-popconfirm>
-<!--      <el-upload action="http://localhost:9090/user/import" :show-file-list="false" accept="xlsx" :on-success="handleExcelImportSuccess" style="display:inline-block">-->
-<!--      <el-button type="primary" class="ml-5">导入 <i class="el-icon-bottom"></i></el-button>-->
-<!--      </el-upload>-->
-<!--      <el-button type="primary" @click="exp" class="ml-5">导出 <i class="el-icon-top"></i></el-button>-->
+      <!--      <el-upload action="http://localhost:9090/user/import" :show-file-list="false" accept="xlsx" :on-success="handleExcelImportSuccess" style="display: inline-block">-->
+      <!--        <el-button type="primary" class="ml-5">导入 <i class="el-icon-bottom"></i></el-button>-->
+      <!--      </el-upload>-->
+      <!--      <el-button type="primary" @click="exp" class="ml-5">导出 <i class="el-icon-top"></i></el-button>-->
     </div>
 
     <el-table :data="tableData" border stripe :header-cell-class-name="'headerBg'"
               row-key="id" default-expand-all @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55"></el-table-column>
-      <el-table-column prop="id" label="ID" width="80px"></el-table-column>
+      <el-table-column prop="id" label="ID" width="80"></el-table-column>
       <el-table-column prop="name" label="名称"></el-table-column>
       <el-table-column prop="path" label="路径"></el-table-column>
       <el-table-column prop="pagePath" label="页面路径"></el-table-column>
-      <el-table-column label="图标" align="center">
+      <el-table-column label="图标" class-name="fontSize18" align="center" label-class-name="fontSize12">
         <template slot-scope="scope">
-          <i :class="scope.row.icon" style="font-size: 20px" />
+          <span :class="scope.row.icon" />
         </template>
       </el-table-column>
       <el-table-column prop="description" label="描述"></el-table-column>
-
-      <el-table-column label="操作" width="300px" align="center">
+      <el-table-column prop="sortNum" label="顺序"></el-table-column>
+      <el-table-column label="操作"  width="300" align="center">
         <template slot-scope="scope">
           <el-button type="primary" @click="handleAdd(scope.row.id)" v-if="!scope.row.pid && !scope.row.path">新增子菜单 <i class="el-icon-plus"></i></el-button>
           <el-button type="success" @click="handleEdit(scope.row)">编辑 <i class="el-icon-edit"></i></el-button>
@@ -61,7 +60,7 @@
       </el-table-column>
     </el-table>
 
-    <el-dialog title="菜单信息" :visible.sync="dialogFormVisible" width="30%">
+    <el-dialog title="菜单信息" :visible.sync="dialogFormVisible" width="30%" >
       <el-form label-width="80px" size="small">
         <el-form-item label="名称">
           <el-input v-model="form.name" autocomplete="off"></el-input>
@@ -73,11 +72,14 @@
           <el-input v-model="form.pagePath" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="图标">
-         <el-select clearable v-model="form.icon" placeholder="请选择" style="width: 100%">
-           <el-option v-for="item in options" :key="item.name" :label="item.name" :value="item.value">
-             <i :class="item.value"/>  {{ item.name}}
-           </el-option>
-         </el-select>
+          <el-select clearable v-model="form.icon" placeholder="请选择" style="width: 100%">
+            <el-option v-for="item in options" :key="item.name" :label="item.name" :value="item.value">
+              <i :class="item.value" /> {{ item.name }}
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="顺序">
+          <el-input v-model="form.sortNum" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="描述">
           <el-input v-model="form.description" autocomplete="off"></el-input>
@@ -92,116 +94,106 @@
 </template>
 
 <script>
+import {serverIp} from "../../public/config";
+
 export default {
-  name: "User",
-  data(){
+  name: "Menu",
+  data() {
     return {
       tableData: [],
       total: 0,
-      pageNum:1,
-      pageSize:10,
-      name:"",
-      form:{},
-      dialogFormVisible:false,
-      multipleSelection:[],
-      options:[]
+      pageNum: 1,
+      pageSize: 10,
+      name: "",
+      form: {},
+      dialogFormVisible: false,
+      multipleSelection: [],
+      options: []
     }
   },
   created() {
     this.load()
   },
-  methods:{
-    load(){
-      //与下面注释的代码对比，我改动的是api的替换和赋值方式的替换
-      this.request.get("/menu",{
-        params:{
-          name:this.name,
+  methods: {
+    load() {
+      this.request.get("/menu", {
+        params: {
+          name: this.name,
         }
-      }).then(res =>{
-        this.tableData=res.data
+      }).then(res => {
+        this.tableData = res.data
       })
-      // fetch("http://localhost:9090/user/page?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&username="+this.username)
-      //     .then(res => res.json()).then(res =>{
-      //   console.log(res)
-      //   //数据绑定
-      //   this.tableData = res.data
-      //   this.total=res.total
-      // })
+
+      // 请求图标的数据
+      this.request.get("/menu/icons").then(res => {
+        this.options = res.data
+      })
     },
-    save(){
-      this.request.post("/menu",this.form).then(res =>{
-        if(res.code ==='200'){
+    save() {
+      this.request.post("/menu", this.form).then(res => {
+        if (res.code === '200') {
           this.$message.success("保存成功")
-          this.dialogFormVisible=false
+          this.dialogFormVisible = false
           this.load()
-        }
-        else {
+        } else {
           this.$message.error("保存失败")
         }
       })
     },
-    handleAdd(pid){
-      this.dialogFormVisible=true
-      this.form={}
-      if(pid){
+    handleAdd(pid) {
+      this.dialogFormVisible = true
+      this.form = {}
+      if (pid) {
         this.form.pid = pid
       }
     },
-    handleEdit(row){
-      //this.form=row     //赋予表单数据,这样有个bug就是，在点击取消的时候表单上仍然有值，不过数据库里面的值没有变化,
-      this.form=JSON.parse(JSON.stringify(row));    //这是解决办法
-      this.dialogFormVisible=true   //打开对话框
-
-      //请求图标的数据
-      this.request.get("/menu/icons").then(res =>{
-        this.options=res.data
-      })
+    handleEdit(row) {
+      this.form = JSON.parse(JSON.stringify(row))
+      this.dialogFormVisible = true
     },
-    del(id){
-      this.request.delete("/menu/"+id).then(res =>{
-        if(res.code ==='200'){
+    del(id) {
+      this.request.delete("/menu/" + id).then(res => {
+        if (res.code === '200') {
           this.$message.success("删除成功")
           this.load()
-        }
-        else {
+        } else {
           this.$message.error("删除失败")
         }
       })
     },
-    handleSelectionChange(val){
+    handleSelectionChange(val) {
       console.log(val)
-      this.multipleSelection=val //获取到的是个对象 不是我们后台需要的ids
+      this.multipleSelection = val
     },
-    delBatch(){
-      let ids = this.multipleSelection.map(v =>v.id)  //[{},{},{}]=>[1,2,3]
-      this.request.post("/menu/del/batch",ids).then(res =>{
-        if(res.code ==='200'){
+    delBatch() {
+      let ids = this.multipleSelection.map(v => v.id)  // [{}, {}, {}] => [1,2,3]
+      this.request.post("/menu/del/batch", ids).then(res => {
+        if (res.code === '200') {
           this.$message.success("批量删除成功")
           this.load()
-        }
-        else {
+        } else {
           this.$message.error("批量删除失败")
         }
       })
     },
-    reset(){
-      this.name=""
+    reset() {
+      this.name = ""
       this.load()
     },
-    handleSizeChange(pageSize){
+    handleSizeChange(pageSize) {
       console.log(pageSize)
-      this.pageSize=pageSize
+      this.pageSize = pageSize
       this.load()
     },
-    handleCurrentChange(pageNum){
+    handleCurrentChange(pageNum) {
       console.log(pageNum)
-      this.pageNum=pageNum
+      this.pageNum = pageNum
       this.load()
     },
-    exp(){
-      window.open("http://localhost:9090/menu/export")
+    exp() {
+      window.open(`http://${serverIp}:9090/role/export`)
     },
-    handleExcelImportSuccess(){
+    handleExcelImportSuccess() {
       this.$message.success("导入成功")
       this.load()
     }
@@ -209,8 +201,15 @@ export default {
 }
 </script>
 
+
 <style>
 .headerBg {
   background: #eee!important;
+}
+.fontSize18{
+  font-size: 18px;
+}
+.fontSize12{
+  font-size: 12px;
 }
 </style>
