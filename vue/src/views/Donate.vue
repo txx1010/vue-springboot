3c1952c5a3572d739db970b8912dad1a846e2273
tangxiang <tangx1105@163.com>
2022-08-26 20:42:56 +0800

    新增页面

---
+++ vue/src/views/Donate.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src='../assets/wx.png' width="300" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Donate"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,209 +1,62 @@
 <template>
-  <div>
-  <el-row :gutter="10" style="margin-bottom: 60px">
-    <el-col :span="6">
-      <el-card style="color: #409EFF">
-        <div><i class="el-icon-user-solid"/> 用户总数</div>
-        <div style="padding: 10px;text-align: center; font-weight: bold">
-            100
-        </div>
-      </el-card>
-    </el-col>
-    <el-col :span="6">
-      <el-card style="color: #67C23A">
-        <div><i class="el-icon-money"/> 销售总量</div>
-        <div style="padding: 10px;text-align: center; font-weight: bold">
-          ￥ 10000000
-        </div>
-      </el-card>
-    </el-col>
-    <el-col :span="6">
-      <el-card style="color: #F56C6C">
-        <div><i class="el-icon-bank-card"></i> 收益总额</div>
-        <div style="padding: 10px;text-align: center; font-weight: bold">
-          ￥ 300000
-        </div>
-      </el-card>
-    </el-col>
-    <el-col :span="6">
-      <el-card style="color: #909399">
-        <div><i class="el-icon-s-shop"></i> 门店总数</div>
-        <div style="padding: 10px;text-align: center; font-weight: bold">
-          20
-        </div>
-      </el-card>
-    </el-col>
+  <div style="color: #666;font-size: 14px;">
+    <div style="padding-bottom: 20px">
+      <b>欢迎你！{{ user.nickname }}</b>
+    </div>
+    <el-card>
+      这是我自学的一个后台管理系统
+      <el-divider />
+      可以用作大作业或者毕业设计的框架<br><br>
+      项目文档是我自己写的 在doc目录下 里面是我在项目里学到的知识点、遇到的问题及解决 以及代码的变更<br><br>
+      在这里特别感谢b站up"程序员青戈"<br><br>
+    </el-card>
+    <div style="height: 1px; background: #ddd; margin: 20px 0"/>
 
-  </el-row>
-
-    <el-row>
+    <el-row :gutter="30">
       <el-col :span="12">
-        <div id="main" style="width: 500px;height: 400px"></div>
+        <div style="padding: 20px 0; font-size: 20px">毕设专用框架</div>
+        <div>
+          这是一款专门针对毕设系统设计的框架，代码简单，结构清晰，如果你是小白，一定不要错过哦
+        </div>
+        <div class="m-10"><el-button type="danger"><i class="el-icon-coin"></i> 免费开源</el-button></div>
+        <div class="m-10"><el-button type="primary"><i class="el-icon-s-custom"></i> 交流wx：tx13087394425</el-button></div>
+        <div class="m-10">
+          <el-button type="primary"><i class="el-icon-link"></i> <a style="color: #fff" href="https://gitee.com/sugar-someone/vue-springboot.git">Gitee源码</a></el-button>
+        </div>
       </el-col>
       <el-col :span="12">
-        <div id="pie" style="width: 500px;height: 400px"></div>
+        <div style="padding: 20px 0; font-size: 20px">技术栈</div>
+        <el-row>
+          <el-col :span="12" style="line-height: 30px">
+            <div><b>后端</b></div>
+            <div>SpringBoot2</div>
+            <div>Hutool</div>
+            <div>Poi</div>
+            <div>Lombok</div>
+            <div>Mybatis/Mybatis-plus</div>
+          </el-col>
+          <el-col :span="12" style="line-height: 30px">
+            <b>前端</b>
+            <div>Vue2</div>
+            <div>Vue-Router</div>
+            <div>VueX</div>
+            <div>ElementUI</div>
+            <div>Axios</div>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
-
 
   </div>
 </template>
 
 <script>
-import * as echarts from 'echarts'
-
-
 export default {
   name: "Home",
   data() {
-    return {}
-
-  },
-  mounted() {//页面元素渲染之后再触发
-
-    var option = {
-      title: {
-        text: '各季度会员数量统计',
-        subtext: '趋势图',
-        left: 'center'
-      },
-      tooltip: {
-        trigger: 'item'
-      },
-      legend: {
-        orient: 'vertical',
-        left: 'left'
-      },
-      xAxis: {
-        type: 'category',
-        data: ["第一季度","第二季度","第三季度","第四季度"]
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          name:"星巴克",
-          data: [],
-          type: 'bar'
-        },
-        {
-          name:"星巴克",
-          data: [],
-          type: 'line'
-        },
-        {
-          name:"瑞幸咖啡",
-          data: [],
-          type: 'bar'
-        },
-        {
-          name:"瑞幸咖啡",
-          data: [],
-          type: 'line'
-        },
-      ]
-    };
-
-
-
-    //饼图
-    var pieoption = {
-      title: {
-        text: '各季度会员数量统计',
-        subtext: '比例图',
-        left: 'center'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
-      },
-      legend: {
-        orient: 'vertical',
-        left: 'left'
-      },
-      series: [
-        {
-          name:'星巴克',
-          type: 'pie',
-          radius: '55%',
-          center: ['25%', '70%'],
-          label:{            //饼图图形上的文本标签
-            normal:{
-              show:true,
-              position:'inner', //标签的位置
-              textStyle : {
-                fontWeight : 300 ,
-                fontSize : 14,   //文字的字体大小
-                color:"#fff"
-              },
-              formatter:'{c}({d}%)'
-            }
-          },
-          data: [], //填空
-
-        },
-        {
-          name:'瑞幸咖啡',
-          type: 'pie',
-          radius: '50%',
-          center: ['75%', '50%'],
-          label:{            //饼图图形上的文本标签
-            normal:{
-              show:true,
-              position:'inner', //标签的位置
-              textStyle : {
-                fontWeight : 300 ,
-                fontSize : 14,   //文字的字体大小
-                color:"#fff"
-              },
-              formatter:'{d}%'
-            }
-          },
-          data: [
-            {name:"第一季度",value:5},
-            {name:"第二季度",value:6},
-            {name:"第三季度",value:7},
-            {name:"第四季度",value:8},
-          ], //填空
-
-        }
-      ]
-    };
-
-    var chartDom = document.getElementById('main');
-    var myChart = echarts.init(chartDom);
-
-    var pieDom = document.getElementById('pie');
-    var pieChart = echarts.init(pieDom);
-
-    this.request.get("/echarts/members").then(res =>{
-      //填空
-      option.xAxis.data = res.data.x;
-      option.series[0].data = res.data
-      option.series[1].data = res.data
-      option.series[2].data = [5,6,7,8]
-      option.series[3].data = [5,6,7,8]
-      //数据准备完毕之后再set
-      myChart.setOption(option);
-
-
-      pieoption.series[0].data = [
-        {name:"第一季度",value:res.data[0]},
-        {name:"第二季度",value:res.data[1]},
-        {name:"第三季度",value:res.data[2]},
-        {name:"第四季度",value:res.data[3]},
-      ]
-      pieChart.setOption(pieoption)
-    })
-
-
-
-
+    return {
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+    }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
